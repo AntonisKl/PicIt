8c1b100af78c949a470c07ae112bd5a48d21ba86
AntonisKl <denti20082008@hotmail.com>
2017-10-14 23:04:05 +0300

    Added history fragment

---
+++ PicIt/app/src/main/java/com/example/android/picit/HistoryFragment.java
@@ -0,0 +1,106 @@
+package com.example.android.picit;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HistoryFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link HistoryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HistoryFragment extends android.app.Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static HistoryFragment newInstance(String param1, String param2) {
+        HistoryFragment fragment = new HistoryFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        ArrayList<HistoryEntry> entries = new ArrayList<HistoryEntry>();
+        entries.add(new HistoryEntry(R.drawable.ic_dashboard_black_24dp, "name", "date"));
+        entries.add(new HistoryEntry(R.drawable.ic_dashboard_black_24dp, "name", "date"));
+        entries.add(new HistoryEntry(R.drawable.ic_dashboard_black_24dp, "name", "date"));
+        entries.add(new HistoryEntry(R.drawable.ic_dashboard_black_24dp, "name", "date"));
+
+        HistoryAdapter adapter = new HistoryAdapter(getActivity(), entries);
+
+        ListView listView = (ListView) view.findViewById(R.id.history_list);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
